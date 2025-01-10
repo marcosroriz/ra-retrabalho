ab1056914dfcde0ab82df9dcf6158db562dddafe
Marcos Roriz <marcosrorizinf@gmail.com>
2025-01-10 14:59:01 -0300

    exemplo de wsgi

---
+++ src/wsgi.sample.py
@@ -0,0 +1,5 @@
+import sys                                                                    
+sys.path.insert(0,"/var/www/ra-retrabalho/src/")                          
+sys.path.insert(0,"/var/www/ra-retrabalho/src/assets")                    
+                                                                             
+from app import server as application
